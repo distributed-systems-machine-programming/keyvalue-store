80e3a530f218319e1ff66ae9296f21eaa886b669
Pulkit Budhiraja <budhi15@gmail.com>
2013-09-29 18:37:58 -0500

    created the runner class

---
+++ distributed_group_mem/src/distributed_group_mem/runner.java
@@ -0,0 +1,13 @@
+package distributed_group_mem;
+
+public class runner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("This will be the runner.");
+
+	}
+
+}
