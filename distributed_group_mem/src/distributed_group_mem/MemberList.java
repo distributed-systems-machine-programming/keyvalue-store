6d514328491143054474f579bb81e590e8d88cd3
Akhilesh <akhilesh890@gmail.com>
2013-10-04 19:06:12 -0500

    Uodated MemberList and included merge function

--- distributed_group_mem/src/distributed_group_mem/MemberList.java
+++ distributed_group_mem/src/distributed_group_mem/MemberList.java
@@ -1,8 +1,8 @@
-package distributed_group_mem;
 
+import java.io.Serializable;
 import java.util.*;
 
-public class MemberList {
+public class MemberList implements Serializable {
 	
 	ArrayList<MemberListEntry> memList = new ArrayList<MemberListEntry>();
 	
@@ -11,6 +11,25 @@ public class MemberList {
 		memList.add(new MemberListEntry (localMachineID));
 		
 	}
+
+	public void incrementHeartBeat(String MachineID) {
+		int localMemberListsize = this.memList.size();
+		int localIndex=0;
+		for (int i = 0; i < localMemberListsize; i++ ) { // if localIndex == -1, entry not found in the local gossip table
+			if (this.memList.get(i).getMachineID().equals(MachineID))
+				localIndex = i;
+			else
+				localIndex = -1;
+		}
+		
+		if (localIndex>=0)
+			this.memList.get(localIndex).incrementHeartBeat();
+		
+	}
+	public int getSize() {
+		return this.memList.size();
+	}
+
 	
 	MemberListEntry findEntry (String MachineID)
 	{
@@ -25,10 +44,44 @@ public class MemberList {
 		return blank;
 	}
 	
-	
-	
-	
+	public void mergeMemberList(MemberList incomingMemberList) {
+		
+		int localIndex=0,otherIndex=0;
+		String currentIP;
+		int localMemberListsize = this.memList.size();
+		int incomingMemberListsize = incomingMemberList.memList.size();
+			
+		for ( otherIndex = 0; otherIndex < incomingMemberListsize; otherIndex++ ) {
+			currentIP = incomingMemberList.memList.get(otherIndex).getMachineID();
 
+			for (int i = 0; i < localMemberListsize; i++ ) { // if localIndex == -1, entry not found in the local gossip table
+				if (this.memList.get(i).getMachineID().equals(currentIP))
+					localIndex = i;
+				else
+					localIndex = -1;
+			}
+
+			if (localIndex >= 0 && incomingMemberList.memList.get(otherIndex).getHeartBeat() > this.memList.get(localIndex).getHeartBeat() && incomingMemberList.memList.get(otherIndex).getDeletionStatus() == false) {
+				if(this.memList.get(localIndex).getDeletionStatus() == true) {
+					this.memList.get(localIndex).setDeletionStatus(false);
+				}			
+				this.memList.get(localIndex).updateHeartBeat(incomingMemberList.memList.get(otherIndex).getHeartBeat());
+				this.memList.get(localIndex).setlocalTimeStamp();
+			}
+
+			else if(localIndex < 0 && incomingMemberList.memList.get(otherIndex).getDeletionStatus() == false) {
+				this.memList.add(new MemberListEntry(currentIP));
+				int currentSize = this.memList.size()-1;
+				this.memList.get(currentSize).setMachineID(currentIP);
+				this.memList.get(currentSize).setHeartBeat(incomingMemberList.memList.get(otherIndex).getHeartBeat());
+				this.memList.get(currentSize).setlocalTimeStamp();  // To be confirmed.
+				this.memList.get(currentSize).setDeletionStatus(false);	
+				
+			}
+		
+		
+	}
 }
-
+	
+}
 
